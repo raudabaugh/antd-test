edd618e2838add0bc319c97b90703f8796b15411
Sam Raudabaugh <sam@threadlearning.com>
2018-11-09 14:44:43 -0500

    Add custom style to Ant button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import Button from 'antd/lib/button';
+import { NativeButtonProps } from 'antd/lib/button/button';
+import styled from 'styled-components';
 import './App.css';
+
+const StyledButton = styled<NativeButtonProps>(props => <Button {...props} />)`
+  color: white;
+  background-color: green;
+`
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button type="primary">Button</Button>
+        <StyledButton>Button</StyledButton>
       </div>
     );
   }
